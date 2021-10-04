8f848dca1728e8f55c1b029d7b8b5367cfbdbb21
Mishary Rafique <misharyrafique01@gmail.com>
2021-10-04 12:55:04 +0600

    about done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import aboutkid from '../../images/about-kid.png';
+import kidabout from '../../images/about-kid-2.png';
 
 const About = () => {
     return (
-        <div>
-            <h2>This Is About</h2>
+        <div className='container'>
+            <h1 className='title text-center'>Why US?</h1>
+            <div class="row">
+                <div class="col-lg-6">
+                    <p style={{ fontFamily: 'Poppins', marginTop: '150px' }}>You will get a quality education with a mesmerizing learning experience. Educapedia provides well-planned career guidelines for your future. Enroll yourself and grab the best programs available on the internet. </p>
+                </div>
+                <div class="col-lg-6">
+                    <img className='w-75' src={aboutkid} alt="" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <img className='w-75' src={kidabout} alt="" />
+                </div>
+                <div class="col-lg-6">
+                    <p style={{ fontFamily: 'Poppins', marginTop: '150px' }}>Educapedia offers world-class programs with proper guidelines. From science to agriculture, everything will be done with hands-on experience. Live with excellence and grab the power pack sensation of education. </p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Teachers/Teachers.js
+++ src/components/Teachers/Teachers.js
@@ -1,9 +1,87 @@
 import React from 'react';
+import mosh from '../../images/teacher-1.jpg';
+import qazi from '../../images/teacher-2.jpg';
+import subeen from '../../images/teacher-3.jpg';
+import jhankar from '../../images/teacher-4.jpg';
+import sumit from '../../images/teacher-5.jpg';
+import jonas from '../../images/teacher-6.jpg';
+import max from '../../images/teacher-7.jpg';
+import angela from '../../images/teacher-8.jpg';
+import hasin from '../../images/teacher-9.jpg';
+import './Teachers.css';
 
 const Teachers = () => {
     return (
-        <div>
-            <h2>This Is Teachers</h2>
+        <div className="container">
+            <div className="teachers row">
+                <div className="col-lg-3">
+                    <img className='w-50' src={mosh} alt="" />
+                    <div className='teacher-info'>
+                        <h4 style={{ fontFamily: 'Poppins' }}>Mosh Hamedani</h4>
+                        <button className='teacher-btn'><a target='_blank' rel='noreferrer' href="https://www.youtube.com/programmingwithmosh">Contact Here</a></button>
+                    </div>
+                </div>
+                <div className="col-lg-3">
+                    <img className='w-50' src={qazi} alt="" />
+                    <div className='teacher-info'>
+                        <h4 style={{ fontFamily: 'Poppins' }}>Rafeh Qazi</h4>
+                        <button className='teacher-btn'><a target='_blank' rel='noreferrer' href="https://www.youtube.com/c/CleverProgrammer">Contact Here</a></button>
+                    </div>
+                </div>
+                <div className="col-lg-3">
+                    <img className='w-50' src={subeen} alt="" />
+                    <div className='teacher-info'>
+                        <h4 style={{ fontFamily: 'Poppins' }}>Tamim Subeen</h4>
+                        <button className='teacher-btn'><a target='_blank' rel='noreferrer' href="https://www.youtube.com/c/TamimShahriar">Contact Here</a></button>
+                    </div>
+                </div>
+            </div>
+            <div className="teachers row">
+                <div className="col-lg-3">
+                    <img className='w-50' src={jhankar} alt="" />
+                    <div className='teacher-info'>
+                        <h4 style={{ fontFamily: 'Poppins' }}>Jhankar Mahbub</h4>
+                        <button className='teacher-btn'><a target='_blank' rel='noreferrer' href="https://www.youtube.com/c/JhankarMahbub">Contact Here</a></button>
+                    </div>
+                </div>
+                <div className="col-lg-3">
+                    <img className='w-50' src={sumit} alt="" />
+                    <div className='teacher-info'>
+                        <h4 style={{ fontFamily: 'Poppins' }}>Sumit Saha</h4>
+                        <button className='teacher-btn'><a target='_blank' rel='noreferrer' href="https://www.youtube.com/c/LearnwithSumit">Contact Here</a></button>
+                    </div>
+                </div>
+                <div className="col-lg-3">
+                    <img className='w-50' src={jonas} alt="" />
+                    <div className='teacher-info'>
+                        <h4 style={{ fontFamily: 'Poppins' }}>Jonas Schmedtmann</h4>
+                        <button className='teacher-btn'><a target='_blank' rel='noreferrer' href="https://www.youtube.com/c/JonasSchmedtmann">Contact Here</a></button>
+                    </div>
+                </div>
+            </div>
+            <div className="teachers row">
+                <div className="col-lg-3">
+                    <img className='w-50' src={max} alt="" />
+                    <div className='teacher-info'>
+                        <h4 style={{ fontFamily: 'Poppins' }}>Maximilian Schwarzmüller</h4>
+                        <button className='teacher-btn'><a target='_blank' rel='noreferrer' href="https://www.youtube.com/c/Academind">Contact Here</a></button>
+                    </div>
+                </div>
+                <div className="col-lg-3">
+                    <img className='w-50' src={angela} alt="" />
+                    <div className='teacher-info'>
+                        <h4 style={{ fontFamily: 'Poppins' }}>Angela Yu</h4>
+                        <button className='teacher-btn'><a target='_blank' rel='noreferrer' href="https://www.udemy.com/user/4b4368a3-b5c8-4529-aa65-2056ec31f37e/">Contact Here</a></button>
+                    </div>
+                </div>
+                <div className="col-lg-3">
+                    <img className='w-50' src={hasin} alt="" />
+                    <div className='teacher-info'>
+                        <h4 style={{ fontFamily: 'Poppins' }}>Hasin Haydar</h4>
+                        <button className='teacher-btn'><a target='_blank' rel='noreferrer' href="https://www.youtube.com/c/LearnwithHasinHayder">Contact Here</a></button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
